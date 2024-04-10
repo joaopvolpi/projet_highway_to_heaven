e2e58d81f9bef327110ebafb16fec3671f7b15a4
lucasvitoriano25 <lucasvitoriano25@gmail.com>
2024-04-10 11:22:01 +0200

    creating race env config

---
+++ racetrack_config.py
@@ -0,0 +1,38 @@
+import gymnasium as gym
+
+env = gym.make("racetrack-v0")
+
+config = {
+    "observation": {
+        "type": "OccupancyGrid",
+        "features": ['presence', 'on_road'],
+        "grid_size": [[-18, 18], [-18, 18]],
+        "grid_step": [3, 3],
+        "as_image": False,
+        "align_to_vehicle_axes": True
+    },
+    "action": {
+        "type": "ContinuousAction",
+        "longitudinal": False,
+        "lateral": True
+    },
+    "simulation_frequency": 15,
+    "policy_frequency": 5,
+    "duration": 300,
+    "collision_reward": -1,
+    "lane_centering_cost": 4,
+    "action_reward": -0.3,
+    "controlled_vehicles": 1,
+    "other_vehicles": 1,
+    "screen_width": 600,
+    "screen_height": 600,
+    "centering_position": [0.5, 0.5],
+    "scaling": 7,
+    "show_trajectories": False,
+    "render_agent": True,
+    "offscreen_rendering": False
+}
+
+
+env.unwrapped.configure(config)
+print(env.reset())
